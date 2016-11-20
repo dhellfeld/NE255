Found mistake in normalization. Behavior now changed. Writeup updated and figures included

Homeworks/HW 5/p2/p2.py
@@ -28,21 +28,24 @@ phi_group1 = np.zeros(N)
 phi_group2 = np.zeros(N)
 phi_group3 = np.zeros(N)
 
+# Outer iteration
 outerconverge = False
 outeritr = 0
 while (outerconverge == False):
 
+    # Calculate current group source values
     groupsource = np.zeros(N)
     for group in range(3):
-        groupsource += q_e[group] + (2./6.) * (sig_s[group,0]*psi_group1.sum(axis=1)) + (sig_s[group,1]*psi_group2.sum(axis=1)) + (sig_s[group,2]*psi_group3.sum(axis=1))
+        groupsource += q_e[group] + (2./18.) * (sig_s[group,0]*psi_group1.sum(axis=1)) + (sig_s[group,1]*psi_group2.sum(axis=1)) + (sig_s[group,2]*psi_group3.sum(axis=1))
 
+    # Loop through groups
     for group in range(3):
 
-        # Initialize center fluxes arrays
+        # Initialize new center fluxes arrays for the curret group
         psi_new = np.zeros((N,6))
         phi_new = np.zeros(N)
 
-        # Iterate
+        # Inner iteratation
         innerconverge = False
         inneritr = 0
         while (innerconverge == False):
@@ -55,26 +58,31 @@ while (outerconverge == False):
 
             # Sweep in mu > 0 (right)
             for i in range(int(N)):
-                s = q_e[group] + (2./6.) * ((sig_s[group,0]*psi_group1[i,:].sum()) + (sig_s[group,1]*psi_group2[i,:].sum()) + (sig_s[group,2]*psi_group3[i,:].sum()))
 
+                # Calculate source
+                # Weight is normalized to two, and there are 18 terms (6 directions in 3 groups)
+                s = q_e[group] + (2./18.) * ((sig_s[group,0]*psi_group1[i,:].sum()) + (sig_s[group,1]*psi_group2[i,:].sum()) + (sig_s[group,2]*psi_group3[i,:].sum()))
+
+                # Calculate cetner flux
                 psi_new[i,0:3] = (s + (2. * np.fabs(mu[0:3]) / (d * (1.+a))) * psi_in) / (sig_t[group] + (2. * np.fabs(mu[0:3]) / (d*(1.+a))))
 
+                # Calcualte outgoing flux (set it equal to incoming flux for next cell)
                 psi_in = ((2. / (1.+a)) * psi_new[i,0:3]) - ((1.-a)/(1.+a))*psi_in
-
 
             # Sweep in mu < 0 (left)
             for i in range(int(N)):
-                s = q_e[group] + (2./6.) * ((sig_s[group,0]*psi_group1[N-i-1,:].sum()) + (sig_s[group,1]*psi_group2[N-i-1,:].sum()) + (sig_s[group,2]*psi_group3[N-i-1,:].sum()))
+                s = q_e[group] + (2./18.) * ((sig_s[group,0]*psi_group1[N-i-1,:].sum()) + (sig_s[group,1]*psi_group2[N-i-1,:].sum()) + (sig_s[group,2]*psi_group3[N-i-1,:].sum()))
 
                 psi_new[N-i-1,3:6] = (s + (2. * np.fabs(mu[3:6]) / (d * (1.+a))) * psi_in) / (sig_t[group] + (2. * np.fabs(mu[3:6]) / (d*(1.+a))))
 
                 psi_in = ((2. / (1.+a)) * psi_new[N-i-1,3:6]) - ((1.-a)/(1.+a))*psi_in
 
-
-            # Calculate scalar flux from angular flux (quaderature)
+            # Calculate scalar flux from angular flux
+            # Sum 6 directions with equal weight
             phi_new = (2./6.) * psi_new.sum(axis=1)
 
             # Calculate convergence criterion (l2 norm of differences)
+            # Update group flux
             if (group == 0):
                 innercrit = np.sqrt(np.sum((phi_new - phi_group1)**2))
                 psi_group1 = np.copy(psi_new)
@@ -89,35 +97,39 @@ while (outerconverge == False):
                 phi_group3 = np.copy(phi_new)
 
             # Check convergence
-            if (innercrit < 0.0001):
+            if (innercrit < 1.0e-4):
                 innerconverge = True
 
-            # Increment iteration number
+            # Increment inner iteration number
             inneritr += 1
 
-        # How many iterations did we do?
+        # How many inner iterations did we do?
         print '(Group = %i) Number of iterations = %i' % (group+1,inneritr)
 
+    # Calculate new groupsource values
     groupsource_new = np.zeros(N)
     for group in range(3):
-        groupsource_new += q_e[group] + (2./6.) * (sig_s[group,0]*psi_group1.sum(axis=1)) + (sig_s[group,1]*psi_group2.sum(axis=1)) + (sig_s[group,2]*psi_group3.sum(axis=1))
+        groupsource_new += q_e[group] + (2./18.) * (sig_s[group,0]*psi_group1.sum(axis=1)) + (sig_s[group,1]*psi_group2.sum(axis=1)) + (sig_s[group,2]*psi_group3.sum(axis=1))
 
+    # Calculate convergence cirterion
     outercrit = np.sqrt(np.sum((groupsource_new - groupsource)**2))
 
+    # Check convergence
     if (outercrit < 1.0e-4):
          outerconverge = True
 
+    # Increment outer iteration number
     outeritr += 1
 
     print 'Outer group interation = %i' % outeritr
 
-# Plot scalar flux
+# Plot scalar fluxes
 plt.figure()
 plt.plot(x,phi_group1, marker='s', color='c',linestyle='none', label='Group 1')
 plt.plot(x,phi_group2, marker='s', color='b',linestyle='none', label='Group 2')
 plt.plot(x,phi_group3, marker='s', color='r',linestyle='none', label='Group 3')
 plt.xlim(-0.1,2.1)
-plt.ylim(0,25)
+plt.ylim(0,8)
 plt.xlabel('x'), plt.ylabel('Center $\phi$')
 plt.title('Cell Center Scalar Flux Profile, $\\alpha$=%.2f' % a)
 plt.legend(numpoints=1,fontsize=10)